fdd464e6aa596b25e9641c2529da9c55142365d4
GAMINDA PREMASIRI <117364246+GAMINDA122@users.noreply.github.com>
2023-10-04 23:56:18 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,11 @@
+from array import *
+
+x = array ('i',[2,-3,5,7,9,10])
+print("1st array : ",x)
+x.append(12)#add only one element after list
+print("2nd array : ",x)
+x.extend([15,17,19])#add multiple value end of the list
+
+print("3rd array : ",x)
+
+
